47885455f269c5a0e7492962cb1c9377d5e0c930
krishnajaadepu <krishnajaadepu05@gmail.com>
2022-12-02 12:23:20 +0530

    thirdperson

--- com/bridgelabz/AddressBook.java
+++ com/bridgelabz/AddressBook.java
@@ -4,7 +4,64 @@ import java.util.ArrayList;
 
 public class AddressBook 
 {
-           public static void main(String[] args) {
-	       System.out.println("Welcome to AddressBook program");
+	 static ArrayList<ContactPerson> contactPersons = new ArrayList<ContactPerson>();
+
+	    static void addPerson(ContactPerson person){
+	        contactPersons.add(person);
+	    }
+
+	    static void editContactPerson(String name, ContactPerson person){
+	        for (int i = 0; i < contactPersons.size(); i++){
+	            if(contactPersons.get(i).firstName == name){
+	                contactPersons.set(i,person);
+	            }
+	        }
+	    }
+
+	    static void showContacts(){
+	        for (int i = 0; i < contactPersons.size(); i++)
+	            System.out.println(contactPersons.get(i).firstName + " " +contactPersons.get(i).lastName + " -> " + contactPersons.get(i).phoneNumber);
+	    }
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to Address Book Program");
+	        ContactPerson krishnaja = new ContactPerson(
+	        		 "krishnaja",
+ 	                "no last name",
+ 	                "adepu",
+ 	                "madhapur",
+ 	                "hyderabad",
+ 	                "100062",
+ 	                "9898989899",
+ 	                "krishnajaadepu@gmail.com" );
+
+	        ContactPerson saiteja = new ContactPerson(
+	        		 "saiteja",
+ 	                " kumar",
+ 	                "pettala",
+ 	                "madhapur",
+ 	                "hyderabad",
+ 	                "100832",
+ 	                "9675339926",
+ 	                "pettalasaiteja11@gmail.com" );
+
+	        ContactPerson rocky = new ContactPerson(
+	                "rocky",
+	                "d",
+	                "dog",
+	                "hanamkonda",
+	                "warangal",
+	                "333990",
+	                "9739211678",
+	                "rockydog@gmail.com" );
+
+	        addPerson(krishnaja);
+	        addPerson(saiteja);
+
+	        showContacts();
+
+	        editContactPerson("saiteja", rocky);
+
+	        showContacts();
+	    }   
 }
-}
+
